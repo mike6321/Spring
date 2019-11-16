559d670bb255570348c13f1c45701665ff7eaf63
choijunwoo <mike6321@naver.com>
2019-11-16 23:57:36 +0900

    Ioc ResourceLoader 활용
    resources에 test.txt 만들어
    1. 해당 파일이 있는지 진위여부 확인
    2.  해당파일의 내용 출력

---
+++ Spring/demospring51resourceloader/src/main/java/me/choi/demospring51resourceloader/Demospring51resourceloaderApplication.java
@@ -0,0 +1,13 @@
+package me.choi.demospring51resourceloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demospring51resourceloaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demospring51resourceloaderApplication.class, args);
+    }
+
+}
---
+++ Spring/demospring51resourceloader/src/test/java/me/choi/demospring51resourceloader/Demospring51resourceloaderApplicationTests.java
@@ -0,0 +1,13 @@
+package me.choi.demospring51resourceloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demospring51resourceloaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
