9b7c08943f9256c592ee9fc175482346cda74251
choijunwoo <mike6321@naver.com>
2019-12-15 15:10:19 +0900

    가게 상세 refactoring

--- SpringBoot/eatgo/eatgo-api/src/main/java/kr/co/choi/eatgo/interfaces/RestaurantController.java
+++ SpringBoot/eatgo/eatgo-api/src/main/java/kr/co/choi/eatgo/interfaces/RestaurantController.java
@@ -1,22 +1,26 @@
 package kr.co.choi.eatgo.interfaces;
 
 import kr.co.choi.eatgo.domin.Restaurant;
+import kr.co.choi.eatgo.domin.RestaurantRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class RestaurantController {
+    private RestaurantRepository repository = new RestaurantRepository();
+
     @GetMapping("/restaurants")
     public List<Restaurant> list() {
-        List<Restaurant> restaurants = new ArrayList<>();
+//        List<Restaurant> restaurants = new ArrayList<>();
 
-        Restaurant restaurant = new Restaurant(1004L,"bob zip~","Seoul");
+//        Restaurant restaurant = new Restaurant(1004L,"bob zip~","Seoul");
+//
+//        restaurants.add(restaurant);
 
-        restaurants.add(restaurant);
+        List<Restaurant> restaurants = repository.findAll();
 
         return restaurants;
     }
@@ -24,22 +28,26 @@ public class RestaurantController {
     @GetMapping("/restaurants/{id}")
     public Restaurant detail(@PathVariable("id") Long id) {
 
-        List<Restaurant> restaurants = new ArrayList<>();
-        restaurants.add(new Restaurant(1004L,"bob zip~","Seoul"));
-        restaurants.add(new Restaurant(2020L,"Cyber Food","Seoul"));
+//        List<Restaurant> restaurants = new ArrayList<>();
+//        restaurants.add(new Restaurant(1004L,"bob zip~","Seoul"));
+//        restaurants.add(new Restaurant(2020L,"Cyber Food","Seoul"));
 
+//        List<Restaurant> restaurants = repository.findAll();
+        Restaurant restaurants = repository.findById(id);
 
-        Restaurant restaurant = restaurants.stream()
-                .filter(r -> r.getId().equals(id))
-                .findFirst()
-                .get();
+        return restaurants;
 
-        if(id == 1004L) {
-            restaurant = new Restaurant(1004L,"bob zip~","Seoul");
-        }
-        if(id == 2020L) {
-            restaurant = new Restaurant(2020L,"Cyber Food","Seoul");
-        }
-        return restaurant;
+//        Restaurant restaurant = restaurants.stream()
+//                .filter(r -> r.getId().equals(id))
+//                .findFirst()
+//                .get();
+
+//        if(id == 1004L) {
+//            restaurants = new Restaurant(1004L,"bob zip~","Seoul");
+//        }
+//        if(id == 2020L) {
+//            restaurants = new Restaurant(2020L,"Cyber Food","Seoul");
+//        }
+
     }
 }
