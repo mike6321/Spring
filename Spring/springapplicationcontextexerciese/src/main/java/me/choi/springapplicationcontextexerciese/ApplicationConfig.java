34da1e4513662acb004d260c4c978a2bef54f8de
choijunwoo <mike6321@naver.com>
2020-01-01 01:10:20 +0900

    @ComponentScan의 등장 : 현재 스프링부트와 가장 가까운 방법

--- Spring/springapplicationcontextexerciese/src/main/java/me/choi/springapplicationcontextexerciese/ApplicationConfig.java
+++ Spring/springapplicationcontextexerciese/src/main/java/me/choi/springapplicationcontextexerciese/ApplicationConfig.java
@@ -1,20 +1,13 @@
 package me.choi.springapplicationcontextexerciese;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 @Configuration
+@ComponentScan(basePackageClasses = DemoApplication.class)
 public class ApplicationConfig {
 
-    @Bean
-    public BookRepository bookRepository() {
-        return new BookRepository();
-    }
 
-
-    @Bean
-    public BookService bookService(BookRepository bookRepository) {
-        //@Autowired를 사용하는 방법
-        return new BookService();
-    }
 }
--- Spring/springapplicationcontextexerciese/src/main/java/me/choi/springapplicationcontextexerciese/BookRepository.java
+++ Spring/springapplicationcontextexerciese/src/main/java/me/choi/springapplicationcontextexerciese/BookRepository.java
@@ -1,4 +1,7 @@
 package me.choi.springapplicationcontextexerciese;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class BookRepository {
 }
