00e8d08b7dc447f9de9075d4ffc767163ba6bac4
choijunwoo <mike6321@naver.com>
2020-01-01 13:43:30 +0900

    Field @Autowired

--- Spring/demospring51/src/main/java/me/choi/demospring51/BookService.java
+++ Spring/demospring51/src/main/java/me/choi/demospring51/BookService.java
@@ -6,10 +6,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class BookService {
 
+    @Autowired(required = false)
     BookRepository bookRepository;
 
-    @Autowired(required = false)
-    public void setBookRepository(BookRepository bookRepository) {
-        this.bookRepository = bookRepository;
-    }
 }
