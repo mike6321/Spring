86aa0fe1c080745e66622df1973144eccbc2c02b
choijunwoo <mike6321@naver.com>
2020-01-01 14:27:30 +0900

    여러개의 빈을 주입받는 방법 List

--- Spring/demospring51/src/main/java/me/choi/demospring51/BookService.java
+++ Spring/demospring51/src/main/java/me/choi/demospring51/BookService.java
@@ -4,13 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class BookService {
 
-    @Autowired @Qualifier("myBookRepository")
-    BookRepository bookRepository;
+    @Autowired
+    List<BookRepository> bookRepository;
 
     public void printBookRepository() {
-        System.out.println(bookRepository.getClass());
+        this.bookRepository.forEach(System.out::println);
     }
 }
--- Spring/demospring51/src/main/java/me/choi/demospring51/JunwooBookRepository.java
+++ Spring/demospring51/src/main/java/me/choi/demospring51/JunwooBookRepository.java
@@ -3,6 +3,6 @@ package me.choi.demospring51;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Repository;
 
-@Repository @Primary
+@Repository
 public class JunwooBookRepository implements BookRepository{
 }
