e66ed52ee56fe2d0f00b23b483c11cb883314094
choijunwoo <mike6321@naver.com>
2020-01-01 16:18:37 +0900

    @ComponentScan의 범위

--- Spring/demospring51/src/main/java/me/choi/demospring51/Demospring51Application.java
+++ Spring/demospring51/src/main/java/me/choi/demospring51/Demospring51Application.java
@@ -1,5 +1,7 @@
 package me.choi.demospring51;
 
+import me.choi.outpackage.MyService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -9,11 +11,11 @@ import java.util.Arrays;
 @SpringBootApplication
 public class Demospring51Application {
 
+	@Autowired
+	MyService myService;
+
+
 	public static void main(String[] args) {
-//		ConfigurableApplicationContext run = SpringApplication.run(Demospring51Application.class, args);
-////		String[] beanDefinitionNames = run.getBeanDefinitionNames();
-////		System.out.println(Arrays.toString(beanDefinitionNames));
 		SpringApplication.run(Demospring51Application.class, args);
 	}
-
 }
---
+++ Spring/demospring51/src/main/java/me/choi/outpackage/MyService.java
@@ -0,0 +1,7 @@
+package me.choi.outpackage;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyService {
+}
