760e2debc9d512383f7538acdbdf321d9599422a
choijunwoo <mike6321@naver.com>
2020-01-01 17:23:25 +0900

    펑션을 사용한 빈 등록

--- Spring/demospring51/src/main/java/me/choi/demospring51/Demospring51Application.java
+++ Spring/demospring51/src/main/java/me/choi/demospring51/Demospring51Application.java
@@ -2,11 +2,16 @@ package me.choi.demospring51;
 
 import me.choi.outpackage.MyService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
 
 import java.util.Arrays;
+import java.util.function.Supplier;
 
 @SpringBootApplication
 public class Demospring51Application {
@@ -16,6 +21,19 @@ public class Demospring51Application {
 
 
 	public static void main(String[] args) {
-		SpringApplication.run(Demospring51Application.class, args);
+//		SpringApplication.run(Demospring51Application.class, args);
+		var app = new SpringApplication(Demospring51Application.class);
+		/*중간에 무엇을 하고 싶을 때*/
+		app.addInitializers((ApplicationContextInitializer<GenericApplicationContext>) ctx ->
+		{
+			/*추가적인 코딩을 할 수 있다.*/
+
+			/*추가적인 코딩을 할 수 있다.*/
+			ctx.registerBean(MyService.class);
+			ctx.registerBean(ApplicationRunner.class, () -> args1 -> System.out.println("Functional Bean Definition"));
+
+		});
+		/*중간에 무엇을 하고 싶을 때*/
+		app.run(args);
 	}
 }
