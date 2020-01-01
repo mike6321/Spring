1ec3b0d03047f707d6e5738cb869a44731cf9cca
choijunwoo <mike6321@naver.com>
2020-01-01 13:39:59 +0900

    Setter @Autowired

--- Spring/demospring51/src/main/java/me/choi/demospring51/BookRepository.java
+++ Spring/demospring51/src/main/java/me/choi/demospring51/BookRepository.java
@@ -2,7 +2,7 @@ package me.choi.demospring51;
 
 import org.springframework.stereotype.Repository;
 
-@Repository
+
 public class BookRepository {
 
 }
--- Spring/demospring51/src/main/java/me/choi/demospring51/Demospring51Application.java
+++ Spring/demospring51/src/main/java/me/choi/demospring51/Demospring51Application.java
@@ -2,12 +2,17 @@ package me.choi.demospring51;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class Demospring51Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Demospring51Application.class, args);
+		ConfigurableApplicationContext run = SpringApplication.run(Demospring51Application.class, args);
+		String[] beanDefinitionNames = run.getBeanDefinitionNames();
+		System.out.println(Arrays.toString(beanDefinitionNames));
 	}
 
 }
