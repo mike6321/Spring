222ab0368022b4114cbcfeec53666443450b71da
choijunwoo <mike6321@naver.com>
2020-01-01 15:33:44 +0900

    autowired의 원리

--- Spring/demospring51/src/main/java/me/choi/demospring51/BookService.java
+++ Spring/demospring51/src/main/java/me/choi/demospring51/BookService.java
@@ -1,18 +1,25 @@
 package me.choi.demospring51;
 
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
 
 @Service
 public class BookService {
 
     @Autowired
-    List<BookRepository> bookRepository;
+    BookRepository junwooBookRepository;
 
-    public void printBookRepository() {
-        this.bookRepository.forEach(System.out::println);
+//    public void printBookRepository() {
+//        System.out.println(this.junwooBookRepository.getClass());
+//    }
+
+    @PostConstruct
+    public void setUp() {
+        System.out.println(this.junwooBookRepository.getClass());
     }
 }
--- Spring/demospring51/src/main/java/me/choi/demospring51/BookServiceRunner.java
+++ Spring/demospring51/src/main/java/me/choi/demospring51/BookServiceRunner.java
@@ -1,8 +1,10 @@
 package me.choi.demospring51;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.AutowiredAnnotationBeanPostProcessor;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,8 +12,14 @@ public class BookServiceRunner implements ApplicationRunner {
     @Autowired
     BookService bookService;
 
+    @Autowired
+    ApplicationContext applicationContext;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        bookService.printBookRepository();
+        //bookService.printBookRepository();
+        AutowiredAnnotationBeanPostProcessor bean = applicationContext.getBean(AutowiredAnnotationBeanPostProcessor.class);
+        System.out.println(bean);
+
     }
 }
