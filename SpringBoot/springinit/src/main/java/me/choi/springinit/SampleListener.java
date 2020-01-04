f66cd91455eb2b7e06f5d2c7ba13c61d6a519e31
choijunwoo <mike6321@naver.com>
2020-01-04 21:07:52 +0900

    Third-party ConfigurationProperties 설정

--- SpringBoot/springinit/src/main/java/me/choi/springinit/SampleListener.java
+++ SpringBoot/springinit/src/main/java/me/choi/springinit/SampleListener.java
@@ -4,13 +4,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.boot.context.event.ApplicationStartingEvent;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SampleListener implements ApplicationRunner {
+    @Bean
+    @ConfigurationProperties("server")
+    public ServerProperties serverProperties () {
+        return new ServerProperties();
+    }
 
     @Autowired
     ChoiProperties choiProperties;
@@ -20,6 +28,7 @@ public class SampleListener implements ApplicationRunner {
         System.out.println("===========================");
         System.out.println(choiProperties.getName());
         System.out.println(choiProperties.getAge());;
+        System.out.println(ServerProperties.Tomcat.Resource.class);
         System.out.println("===========================");
     }
 }
