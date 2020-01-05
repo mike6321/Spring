c4fc46d2b9c83325a768020e2d1a658c848f95b7
choijunwoo <mike6321@naver.com>
2020-01-05 13:21:22 +0900

    mockBean을 사용하여 Service단 분리

--- SpringBoot/springtestdemo/src/test/java/me/choi/springtestdemo/SampleControllerTest.java
+++ SpringBoot/springtestdemo/src/test/java/me/choi/springtestdemo/SampleControllerTest.java
@@ -3,11 +3,13 @@ package me.choi.springtestdemo;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 @ExtendWith({SpringExtension.class})
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -16,8 +18,12 @@ public class SampleControllerTest {
     @Autowired
     TestRestTemplate testRestTemplate;
 
+    @MockBean
+    SampleService mockSampleService;
+
     @Test
     public void hello() throws Exception {
+        when(mockSampleService.getName()).thenReturn("junwoo");
         String result = testRestTemplate.getForObject("/hello", String.class);
         assertEquals(result,"hellojunwoo");
     }
