2e878c50918f71c5491a2f6eb54d65ca388c7a6f
choijunwoo <mike6321@naver.com>
2020-01-11 00:42:54 +0900

    static resources

--- SpringBoot/SpringMvc/src/main/java/me/choi/demospringmvc/config/WebConfig.java
+++ SpringBoot/SpringMvc/src/main/java/me/choi/demospringmvc/config/WebConfig.java
@@ -2,10 +2,15 @@ package me.choi.demospringmvc.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
-
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/m/**")
+                .addResourceLocations("classpath:/m/")
+                .setCachePeriod(20);
+    }
 }
