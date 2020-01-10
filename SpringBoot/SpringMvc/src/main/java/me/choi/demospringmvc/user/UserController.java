b6906d4305198cb3959bca38074d469df5d69e3a
choijunwoo <mike6321@naver.com>
2020-01-10 21:00:05 +0900

    Spring Mvc(2) - HttpMessageConverters

--- SpringBoot/SpringMvc/src/main/java/me/choi/demospringmvc/user/UserController.java
+++ SpringBoot/SpringMvc/src/main/java/me/choi/demospringmvc/user/UserController.java
@@ -1,13 +1,19 @@
 package me.choi.demospringmvc.user;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
 
-    @GetMapping("/hello")
-    public String hello() {
+@GetMapping("/hello")
+    /*@ResponseBody 가 생략되어 있다.*/
+    public @ResponseBody String hello() {
         return "hello";
+    }
+
+    @PostMapping("/users/create")
+    public User create(@RequestBody User user) {
+
+        return user;
     }
 }
--- SpringBoot/SpringMvc/src/test/java/me/choi/demospringmvc/user/UserControllerTest.java
+++ SpringBoot/SpringMvc/src/test/java/me/choi/demospringmvc/user/UserControllerTest.java
@@ -1,15 +1,19 @@
 package me.choi.demospringmvc.user;
 
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(UserController.class)
@@ -23,5 +27,24 @@ public class UserControllerTest {
         mockMvc.perform(get("/hello"))
                 .andExpect(status().isOk())
                 .andExpect(content().string("hello"));
+    }
+
+    @Test
+    public void createUser_JSON() throws Exception {
+        //String userJson = "{\"userrname\":\"choi\", \}";
+        JSONObject userJson = new JSONObject();
+        userJson.put("username", "choi");
+        userJson.put("password", "123");
+        userJson.toString();
+
+        mockMvc.perform(post("/users/create")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(String.valueOf(userJson)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.username",is(equalTo("choi"))))
+                .andExpect(jsonPath("$.password",is(equalTo("123"))))
+        ;
+
     }
 }
