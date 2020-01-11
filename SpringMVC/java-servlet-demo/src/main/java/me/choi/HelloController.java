f537098fdbf8d3ce0a17baa15e6e1fd3771f7a02
choijunwoo <mike6321@naver.com>
2020-01-11 23:49:21 +0900

    @RestController

---
+++ SpringMVC/java-servlet-demo/src/main/java/me/choi/HelloController.java
@@ -0,0 +1,19 @@
+package me.choi;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @Autowired
+    HelloService helloService;
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello, "+helloService.getName();
+    }
+}
