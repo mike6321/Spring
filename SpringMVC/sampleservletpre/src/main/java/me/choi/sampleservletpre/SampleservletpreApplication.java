77b5009e639e724fb425fdc5153f80c35b62ac46
choijunwoo <mike6321@naver.com>
2020-01-11 13:38:15 +0900

    (SPRING MVC) Servlet 개요

---
+++ SpringMVC/sampleservletpre/src/main/java/me/choi/sampleservletpre/SampleservletpreApplication.java
@@ -0,0 +1,13 @@
+package me.choi.sampleservletpre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleservletpreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleservletpreApplication.class, args);
+	}
+
+}
---
+++ SpringMVC/sampleservletpre/src/test/java/me/choi/sampleservletpre/SampleservletpreApplicationTests.java
@@ -0,0 +1,13 @@
+package me.choi.sampleservletpre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleservletpreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
