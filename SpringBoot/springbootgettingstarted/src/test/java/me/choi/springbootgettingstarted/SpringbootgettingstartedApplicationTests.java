abf5f60dc9de2cd4086cafed3c36c957fd3136e6
choijunwoo <mike6321@naver.com>
2020-01-12 17:21:43 +0900

    SpringBoot 자동설정-@EnableAutoConfiguration,@SpringBootApplication제거

---
+++ SpringBoot/springbootgettingstarted/src/test/java/me/choi/springbootgettingstarted/SpringbootgettingstartedApplicationTests.java
@@ -0,0 +1,13 @@
+package me.choi.springbootgettingstarted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootgettingstartedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
