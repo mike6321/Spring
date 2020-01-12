0a73f086d738a8e9b9f4e251f6d454b27beb302b
choijunwoo <mike6321@naver.com>
2020-01-12 16:14:16 +0900

    springboot 의존성관리

--- SpringBoot/springinit/src/main/java/me/choi/springinit/SpringinitApplication.java
+++ SpringBoot/springinit/src/main/java/me/choi/springinit/SpringinitApplication.java
@@ -1,17 +1,13 @@
 package me.choi.springinit;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(ChoiProperties.class)
 public class SpringinitApplication {
 
-	public static void main(String[] args) {
-		SpringApplication springApplication = new SpringApplication(SpringinitApplication.class);
-		springApplication.setWebApplicationType(WebApplicationType.NONE);
-		springApplication.run(args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringinitApplication.class, args);
+    }
+
 }
