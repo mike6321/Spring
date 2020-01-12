fdd602014f3fb3c5c6a727e825ef498929a943f2
choijunwoo <mike6321@naver.com>
2020-01-13 01:03:28 +0900

    @ConditionalOnMissingBean(junwoo-spring-boot-starter project)

--- SpringBoot/junwoo-spring-boot-starter/src/main/java/me/choi/HolomanConfiguration.java
+++ SpringBoot/junwoo-spring-boot-starter/src/main/java/me/choi/HolomanConfiguration.java
@@ -1,11 +1,13 @@
 package me.choi;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class HolomanConfiguration {
     @Bean
+    @ConditionalOnMissingBean
     public Holoman holoman() {
         Holoman holoman = new Holoman();
         holoman.setHowLong(5);
