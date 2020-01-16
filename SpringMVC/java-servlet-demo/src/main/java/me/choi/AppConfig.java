3ccb81255e4adb1aba5756a3fee7d511043a4db4
choijunwoo <mike6321@naver.com>
2020-01-16 20:50:05 +0900

    conflict solution

--- SpringMVC/java-servlet-demo/src/main/java/me/choi/AppConfig.java
+++ SpringMVC/java-servlet-demo/src/main/java/me/choi/AppConfig.java
@@ -2,10 +2,9 @@ package me.choi;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Controller;
 
 @Configuration
-@ComponentScan(excludeFilters = @ComponentScan.Filter(Controller.class))
+@ComponentScan
 public class AppConfig {
 
 }
