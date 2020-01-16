f03930d4fabeba3e7d3c11ba04b83e5aac4dad3a
choijunwoo <mike6321@naver.com>
2020-01-16 20:47:34 +0900

    conflict solution

--- SpringBoot/webServer/src/main/java/me/choi/webserver/WebServerApplication.java
+++ SpringBoot/webServer/src/main/java/me/choi/webserver/WebServerApplication.java
@@ -1,38 +1,12 @@
 package me.choi.webserver;
 
-import org.apache.catalina.Context;
-import org.apache.catalina.LifecycleException;
-import org.apache.catalina.startup.Tomcat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
+@SpringBootApplication
 public class WebServerApplication {
 
-public static void main(String[] args) throws LifecycleException {
-
-        Tomcat tomcat = new Tomcat();
-        tomcat.setPort(8080);
-        Context context = tomcat.addContext("","/");
-
-        /*서블릿 작성*/
-        HttpServlet servlet = new HttpServlet() {
-            @Override
-            protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                System.out.println("doGet");
-            }
-        };
-        tomcat.addServlet("", "servlet", servlet);
-        context.addServletMappingDecoded("/hello", "servlet");
-        /*서블릿 작성*/
-
-        tomcat.getConnector();
-        tomcat.start();
+public static void main(String[] args) {
+        SpringApplication.run(WebServerApplication.class, args);
     }
-
 }
