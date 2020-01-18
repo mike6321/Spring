4cb339ac67147132e09e6f57fc5f7e5368750ae3
choijunwoo <mike6321@naver.com>
2020-01-18 18:02:40 +0900

    dispatcherservlet의 이해 view return

--- SpringMVC/java-servlet-demo/src/main/java/me/choi/HelloController.java
+++ SpringMVC/java-servlet-demo/src/main/java/me/choi/HelloController.java
@@ -6,14 +6,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 
     @Autowired
     HelloService helloService;
 
     @GetMapping("/hello")
+    @ResponseBody
     public String hello() {
         return "hello, "+helloService.getName();
+    }
+
+    @GetMapping("/sample")
+    public String sample() {
+        return "/WEB-INF/sample.jsp";
     }
 }
