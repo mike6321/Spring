7b90603f1d34c429e6b9ea0934cf47991e85128d
choijunwoo <mike6321@naver.com>
2020-01-18 23:52:47 +0900

    새 프로젝트 생성

---
+++ SpringBoot/web-server-show-case/src/main/java/me/choi/webservershowcase/WebServerShowCaseApplication.java
@@ -0,0 +1,13 @@
+package me.choi.webservershowcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServerShowCaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServerShowCaseApplication.class, args);
+	}
+
+}
---
+++ SpringBoot/web-server-show-case/src/test/java/me/choi/webservershowcase/WebServerShowCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package me.choi.webservershowcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServerShowCaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
