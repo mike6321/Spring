ecd0083f80b81fc904c87771f15471f3bdbefe1b
choijunwoo <mike6321@naver.com>
2020-02-01 21:25:32 +0900

    web.xml 없이 동작시키는 방법

--- SpringMVC/java-servlet-demo/src/main/java/me/choi/HelloController.java
+++ SpringMVC/java-servlet-demo/src/main/java/me/choi/HelloController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class HelloController {
@@ -12,7 +11,7 @@ public class HelloController {
     @Autowired
     HelloService helloService;
 
-    @GetMapping("/hello/{id}")
+    @GetMapping("/hello")
     @ResponseBody
     public String hello() {
         return "hello, "+helloService.getName();
--- SpringMVC/java-servlet-demo/src/main/java/me/choi/WebConfig.java
+++ SpringMVC/java-servlet-demo/src/main/java/me/choi/WebConfig.java
@@ -4,17 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.HandlerAdapter;
 import org.springframework.web.servlet.HandlerMapping;
 import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.mvc.method.annotation.RequestBodyAdviceAdapter;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
-@ComponentScan(useDefaultFilters = false, includeFilters = @ComponentScan.Filter(Controller.class))
+@ComponentScan
 public class WebConfig {
 
     @Bean
@@ -28,7 +26,6 @@ public class WebConfig {
     @Bean
     public HandlerAdapter handlerAdapter() {
         RequestMappingHandlerAdapter handlerAdapter = new RequestMappingHandlerAdapter();
-        handlerAdapter.setArgumentResolvers();
         return handlerAdapter;
     }
     @Bean
