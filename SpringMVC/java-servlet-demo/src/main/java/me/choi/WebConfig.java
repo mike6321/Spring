678939f9bafaeee1646468d958eb798907d5cb7f
junwoo <33277588+mike6321@users.noreply.github.com>
2020-02-02 20:28:13 +0900

    WebMvcConfigurer의 사용 with @EnableWebMvc

--- SpringMVC/java-servlet-demo/src/main/java/me/choi/WebConfig.java
+++ SpringMVC/java-servlet-demo/src/main/java/me/choi/WebConfig.java
@@ -1,39 +1,17 @@
 package me.choi;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
-import org.springframework.web.servlet.HandlerAdapter;
-import org.springframework.web.servlet.HandlerMapping;
-import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
-import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @ComponentScan
-public class WebConfig {
-
-    @Bean
-    public HandlerMapping handlerMapping() {
-        RequestMappingHandlerMapping handlerMapping = new RequestMappingHandlerMapping();
-        handlerMapping.setInterceptors();
-        handlerMapping.setOrder(Ordered.HIGHEST_PRECEDENCE);
-        return handlerMapping;
-    }
-
-    @Bean
-    public HandlerAdapter handlerAdapter() {
-        RequestMappingHandlerAdapter handlerAdapter = new RequestMappingHandlerAdapter();
-        return handlerAdapter;
-    }
-    @Bean
-    public ViewResolver viewResolver() {
-        InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
-        internalResourceViewResolver.setPrefix("/WEB-INF/");
-        internalResourceViewResolver.setSuffix(".jsp");
-
-        return internalResourceViewResolver;
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.jsp("/WEB-INF/",".jsp");
     }
 }
