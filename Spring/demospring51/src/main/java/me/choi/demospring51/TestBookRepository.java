f07e7c74d0df7dc48aab1d145ea64d42c9c7e782
junwoo <33277588+mike6321@users.noreply.github.com>
2020-02-13 23:57:04 +0900

    IoC 컨테이너 - Environment_프로파일(2)

--- Spring/demospring51/src/main/java/me/choi/demospring51/TestBookRepository.java
+++ Spring/demospring51/src/main/java/me/choi/demospring51/TestBookRepository.java
@@ -1,5 +1,8 @@
 package me.choi.demospring51;
 
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Repository;
+
 /**
  * Project : demospring51
  * Created by InteliJ IDE
@@ -7,5 +10,7 @@ package me.choi.demospring51;
  * Date : 13/02/2020
  * Time : 11:32 오후
  */
+@Repository
+@Profile("!prod")
 public class TestBookRepository implements BookRepository{
 }
