73a4bbe1454e44f29b5b99bf27d88b5894a9c0a1
junwoo <33277588+mike6321@users.noreply.github.com>
2020-04-06 22:43:20 +0900

    configuration enhancer 만들지 못했다

---
+++ spring-core/src/main/java/me/choi/springcore/spring_enhancer/spring_cglib_at_configuration/Config.java
@@ -0,0 +1,14 @@
+package me.choi.springcore.spring_enhancer.spring_cglib_at_configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Project : spring-core
+ * Created by InteliJ IDE
+ * Developer : junwoochoi
+ * Date : 2020/04/06
+ * Time : 10:20 오후
+ */
+@Configuration
+public class Config {
+}
---
+++ spring-core/src/main/java/me/choi/springcore/spring_enhancer/spring_cglib_at_configuration/EnhancerExercise.java
@@ -0,0 +1,18 @@
+package me.choi.springcore.spring_enhancer.spring_cglib_at_configuration;
+
+import org.springframework.context.annotation.ConfigurationClassEnhancer;
+
+/**
+ * Project : spring-core
+ * Created by InteliJ IDE
+ * Developer : junwoochoi
+ * Date : 2020/04/06
+ * Time : 10:35 오후
+ */
+public class EnhancerExercise {
+
+    public void enhanceConfigurationClass() {
+
+
+    }
+}
