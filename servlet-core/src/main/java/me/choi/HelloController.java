f8f8272a5cfe300cf8add29d647de4c8c11e01c5
junwoo <33277588+mike6321@users.noreply.github.com>
2020-04-12 23:47:00 +0900

    ResponseBody 없이 문자열만 리턴하는 경우

---
+++ servlet-core/src/main/java/me/choi/HelloController.java
@@ -0,0 +1,11 @@
+package me.choi;
+
+/**
+ * Project : servlet-core
+ * Created by InteliJ IDE
+ * Developer : junwoochoi
+ * Date : 2020/04/12
+ * Time : 7:41 오후
+ */
+public class HelloController {
+}
