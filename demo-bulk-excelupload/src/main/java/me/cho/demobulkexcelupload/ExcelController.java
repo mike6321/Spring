d29f6be23d8a3c5e7c924d957930f785bceb897d
junwoo <33277588+mike6321@users.noreply.github.com>
2020-04-14 22:19:20 +0900

    bulk excelupload 프론트 생성

--- demo-bulk-excelupload/src/main/java/me/cho/demobulkexcelupload/ExcelController.java
+++ demo-bulk-excelupload/src/main/java/me/cho/demobulkexcelupload/ExcelController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import java.util.Iterator;
@@ -25,7 +26,7 @@ public class ExcelController {
         return "excel";
     }
 
-    @RequestMapping(value = "/ExcelController")
+    @RequestMapping(value = "/ExcelController", method = RequestMethod.POST)
     public String excelUpload(MultipartHttpServletRequest request, Model model) {
         System.out.println("???????");
 
@@ -33,4 +34,33 @@ public class ExcelController {
 
         return "excel";
     }
+
+    @ResponseBody
+    @RequestMapping(value = "/excelUploadAjax", method = RequestMethod.POST)
+    public String excelUploadAjax(MultipartHttpServletRequest request)  throws Exception{
+        MultipartFile excelFile =request.getFile("excelFile");
+        System.out.println("엑셀 파일 업로드 컨트롤러");
+        System.out.println(excelFile);
+////        if(excelFile==null || excelFile.isEmpty()){
+////            throw new RuntimeException("엑셀파일을 선택 해 주세요.");
+////        }
+////
+////        File destFile = new File("D:\\"+excelFile.getOriginalFilename());
+////        try{
+////            excelFile.transferTo(destFile);
+////        }catch(IllegalStateException | IOException e){
+////            throw new RuntimeException(e.getMessage(),e);
+////        }
+////
+////        userService.excelUpload(destFile);
+////
+////        //FileUtils.delete(destFile.getAbsolutePath());
+////
+////        ModelAndView view = new ModelAndView();
+////        view.setViewName("");
+////        return view;
+        return null;
+    }
+
+
 }
