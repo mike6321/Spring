07aabad17327aa20a4c856c8474a8dd1915cf2ac
junwoo <33277588+mike6321@users.noreply.github.com>
2020-04-18 19:26:10 +0900

    web.xml 제거

---
+++ servlet-core/src/main/java/me/choi/WebApplication.java
@@ -0,0 +1,26 @@
+package me.choi;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+/**
+ * Project : servlet-core
+ * Created by InteliJ IDE
+ * Developer : junwoochoi
+ * Date : 2020/04/18
+ * Time : 7:23 오후
+ */
+public class WebApplication implements WebApplicationInitializer {
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.setServletContext(servletContext);
+        context.register(WebConfig.class);
+        context.refresh();
+
+    }
+}
