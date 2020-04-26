832a92d17003cb61ea37ded2d537e5c53b4f3521
choijunwoo <mike6321@naver.com>
2020-04-26 14:47:45 +0900

    fetch

--- SpringBoot/DDD-master/fastcampus.ddd-alone-master/ddd-alone-4/src/main/java/io/github/wotjd243/pokemon/pokemon/domain/Pokemon.java
+++ SpringBoot/DDD-master/fastcampus.ddd-alone-master/ddd-alone-4/src/main/java/io/github/wotjd243/pokemon/pokemon/domain/Pokemon.java
@@ -1,20 +1,44 @@
 package io.github.wotjd243.pokemon.pokemon.domain;
 
-import javax.validation.constraints.Max;
+import java.util.Objects;
 
 public class Pokemon {
-
-    private final PokedexNumber pokedexNumber;
+    private final NationalPokedexNumber number;
     private final String name;
-    
+    private final CaptureRate captureRate;
 
-    public Pokemon(final int pokedexNumber, String name) {
-        this(new PokedexNumber(pokedexNumber), name);
+    public Pokemon(final int number, final String name, final int captureRate) {
+        this(NationalPokedexNumber.valueOf(number), name, CaptureRate.valueOf(captureRate));
     }
 
-    //주생성자
-    public Pokemon(final PokedexNumber pokedexNumber, final String name) {
-        this.pokedexNumber = pokedexNumber;
+    public Pokemon(final NationalPokedexNumber number, final String name, final CaptureRate captureRate) {
+        this.number = number;
         this.name = name;
+        this.captureRate = captureRate;
+    }
+
+    public int getNumber() {
+        return number.toInt();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCaptureRate() {
+        return captureRate.toInt();
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final Pokemon pokemon = (Pokemon) o;
+        return number.equals(pokemon.number);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
     }
 }
