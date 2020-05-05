9a217e9d692eff169bd5451555cbe5acf74ff419
junwoo <33277588+mike6321@users.noreply.github.com>
2020-05-05 23:42:17 +0900

    web.xml 제거

---
+++ servlet-core/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,13 @@
+
+T
+$src/main/java/me/choi/WebConfig.java,e/b/eb045772da1f9477c46b05b33eb38112ba48388e
+Y
+)src/main/java/me/choi/WebApplication.java,4/1/41f450e13eb9dc1c899f446e3a8a888cd08a387d
+U
+%src/main/java/me/choi/MyListener.java,4/1/41cb695256422aa9f20e77e83a2372e052bea507
+W
+'src/main/java/me/choi/HelloServlet.java,4/6/46c54c055e83359f8849bbec61e37d2186ae81db
+W
+'src/main/java/me/choi/HelloService.java,a/2/a21dc16fd41b1b5317dd31f236bad1e08fc49204
+Z
+*src/main/java/me/choi/HelloController.java,4/3/43601f0ce832536c0ea8679863da219e40d6be3c
--- servlet-core/src/main/java/me/choi/WebApplication.java
+++ servlet-core/src/main/java/me/choi/WebApplication.java
@@ -1,11 +1,12 @@
 package me.choi;
 
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
 
 /**
  * Project : servlet-core
@@ -15,11 +16,19 @@ import javax.servlet.ServletException;
  * Time : 7:23 오후
  */
 
-public class WebApplication {//implements WebApplicationInitializer {
+public class WebApplication implements WebApplicationInitializer {
 
+    @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.setServletContext(servletContext);
+        context.register(WebConfig.class);
+        context.refresh();
 
-//public class WebApplication implements WebApplicationInitializer {
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(context);
+        ServletRegistration.Dynamic registration = servletContext.addServlet("app", dispatcherServlet);
+
+        registration.addMapping("/app/*");
 
     }
 }
