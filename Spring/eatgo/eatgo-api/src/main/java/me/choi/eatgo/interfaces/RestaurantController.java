985909249675f0d2d29413c0233f493dcfe81fa2
junwoo <33277588+mike6321@users.noreply.github.com>
2020-06-13 18:54:47 +0900

    가게상세정보 리스트로 관리

--- Spring/eatgo/eatgo-api/src/main/java/me/choi/eatgo/interfaces/RestaurantController.java
+++ Spring/eatgo/eatgo-api/src/main/java/me/choi/eatgo/interfaces/RestaurantController.java
@@ -2,6 +2,7 @@ package me.choi.eatgo.interfaces;
 
 import me.choi.eatgo.domain.Restaurant;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -25,6 +26,22 @@ public class RestaurantController {
         restaurants.add(restaurant);
 
         return restaurants;
+    }
+
+    @GetMapping("/restaurant/{id}")
+    public Restaurant detail(@PathVariable("id") Long id) {
+
+        List<Restaurant> restaurants = new ArrayList<>();
+
+        restaurants.add(new Restaurant(id,"Bob zip", "Suweon"));
+        restaurants.add(new Restaurant(id,"Cyber Food", "Suweon"));
+
+        Restaurant restaurant = restaurants.stream()
+                                            .filter(r -> r.getId().equals(id))
+                                            .findFirst()
+                                            .orElse(null);
+
+        return restaurant;
     }
 
 }
