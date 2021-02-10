da1e1e8abc6d5b70e1181f4c1a763c2cab5baa69
junwoo <33277588+mike6321@users.noreply.github.com>
2021-02-10 17:39:01 +0900

    SpringCore with gradle init

---
+++ Spring/spring-core-review/src/main/java/me/choi/springcorereview/SpringCoreReviewApplication.java
@@ -0,0 +1,13 @@
+package me.choi.springcorereview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoreReviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCoreReviewApplication.class, args);
+    }
+
+}
---
+++ Spring/spring-core-review/src/test/java/me/choi/springcorereview/SpringCoreReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package me.choi.springcorereview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoreReviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
