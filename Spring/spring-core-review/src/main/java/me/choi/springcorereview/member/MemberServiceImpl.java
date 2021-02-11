9835087a04827ef8231e4d07478332a05bf18957
choijunwoo <mike6321@naver.com>
2021-02-11 16:57:38 +0900

    todo 추가

--- Spring/spring-core-review/src/main/java/me/choi/springcorereview/member/MemberServiceImpl.java
+++ Spring/spring-core-review/src/main/java/me/choi/springcorereview/member/MemberServiceImpl.java
@@ -8,7 +8,7 @@ package me.choi.springcorereview.member;
  * Time : 4:37 오후
  */
 public class MemberServiceImpl implements MemberService{
-
+    // TODO: 2021/02/11 실제 할당하는 부분이 구현체를 알고있어야 한다. 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
     @Override
