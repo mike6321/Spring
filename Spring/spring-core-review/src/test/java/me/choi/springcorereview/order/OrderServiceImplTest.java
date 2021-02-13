53393b0021901b2cbd5e799febc4f40d1b14b381
junwoo <33277588+mike6321@users.noreply.github.com>
2021-02-13 22:11:58 +0900

    생성자주입, 필드주입, 필드주입 테스트코드 TODO 작성

---
+++ Spring/spring-core-review/src/test/java/me/choi/springcorereview/order/OrderServiceImplTest.java
@@ -0,0 +1,40 @@
+package me.choi.springcorereview.order;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Project : spring-core-review
+ *
+ * @author : jwdeveloper
+ * @comment :
+ * Time : 9:59 오후
+ */
+public class OrderServiceImplTest {
+
+    // TODO: [setter 주입 테스트코드 작성] junwoochoi 2021/02/13 10:00 오후
+    /**
+     * 
+     * */  
+    @Test
+    void createSetter() {
+
+    }
+
+    // TODO: [필드 주입 테스트 코드 작성] junwoochoi 2021/02/13 10:07 오후
+    /**
+     *
+     * */
+    @Test
+    void createField() {
+
+    }
+
+    // TODO: [생성자 주입 테스트 코드 작성] junwoochoi 2021/02/13 10:07 오후
+    /**
+     *
+     * */
+    @Test
+    void createConstructor() {
+
+    }
+}
