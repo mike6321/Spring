074730fcec9d824898251a01fdfcd0507faf5840
junwoo <33277588+mike6321@users.noreply.github.com>
2021-02-13 23:24:54 +0900

    FixDisCountPolicy Bean으로 등록 시 오류

--- Spring/spring-core-review/src/main/java/me/choi/springcorereview/discount/FixDiscountPolicy.java
+++ Spring/spring-core-review/src/main/java/me/choi/springcorereview/discount/FixDiscountPolicy.java
@@ -2,6 +2,7 @@ package me.choi.springcorereview.discount;
 
 import me.choi.springcorereview.member.Grade;
 import me.choi.springcorereview.member.Member;
+import org.springframework.stereotype.Component;
 
 /**
  * Project : spring-core-review
@@ -10,6 +11,8 @@ import me.choi.springcorereview.member.Member;
  * @comment :
  * Time : 5:08 오후
  */
+// TODO: FixDisCountPolicy도 빈으로 등록  
+@Component
 public class FixDiscountPolicy implements DiscountPolicy{
     private int discountFixAmount = 1000; // 1000원 할인
     @Override
