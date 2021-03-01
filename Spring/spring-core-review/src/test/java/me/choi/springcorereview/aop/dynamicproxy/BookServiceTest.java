9043998609ed79169c549dc253a4953ec016a232
junwoo <33277588+mike6321@users.noreply.github.com>
2021-03-02 00:21:39 +0900

    DynamicProxy2

--- Spring/spring-core-review/src/test/java/me/choi/springcorereview/aop/dynamicproxy/BookServiceTest.java
+++ Spring/spring-core-review/src/test/java/me/choi/springcorereview/aop/dynamicproxy/BookServiceTest.java
@@ -22,11 +22,21 @@ class BookServiceTest {
                 BookService bookService = new RealSubjectBookService();
                 @Override
                 public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-                    System.out.println("****************logging****************");
-                    Object invoke = method.invoke(bookService, args);
-                    System.out.println("****************logging****************");
+                    // TODO: rent에만 적용할 수 있게끔 적용하였으나 코드의 복잡성이 올라간다. 2021/03/02 12:21 오전
+                    if (method.getName().equals("rent")) {
+                        System.out.println("****************logging****************");
+                        Object invoke = method.invoke(bookService, args);
+                        System.out.println("****************logging****************");
 
-                    return invoke;
+                        return invoke;
+                    }
+
+                    // TODO: 현재 rent에만 로깅을 적용하고 싶었으나 returnBook에도 로깅이 적용이 되었다. 2021/03/02 12:19 오전
+//                    System.out.println("****************logging****************");
+//                    Object invoke = method.invoke(bookService, args);
+//                    System.out.println("****************logging****************");
+
+                    return method.invoke(bookService, args);
                 }
             });
 
@@ -35,7 +45,6 @@ class BookServiceTest {
         //given
         Book book = new Book(1, "Toby Spring");
         //when
-        // TODO: 현재 rent에만 로깅을 적용하고 싶었으나 returnBook에도 로깅이 적용이 되었다. 2021/03/02 12:19 오전
         bookService.rent(book);
         bookService.returnBook(book);
         //then
