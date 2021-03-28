15eaeb6e333564c8ed1ab97df665dcc9d50ea93b
junwoo <33277588+mike6321@users.noreply.github.com>
2021-03-28 13:48:12 +0900

    add dependency custom config

--- SpringBoot/Uwahan-spring-boot/src/main/java/me/choi/HelloController.java
+++ SpringBoot/Uwahan-spring-boot/src/main/java/me/choi/HelloController.java
@@ -1,8 +1,21 @@
-package me.choi;/**
+package me.choi;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
  * Project : Uwahan-spring-boot
- * 
+ *
  * @author : jwdeveloper
- * @comment : 
+ * @comment :
  * Time : 1:05 오후
-*/public class HelloController {
+ */
+@Controller
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "index";
+    }
 }
