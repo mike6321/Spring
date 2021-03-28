665168a3d35ac5fe03899bce9449f2d3ea09f36f
junwoo <33277588+mike6321@users.noreply.github.com>
2021-03-28 12:54:07 +0900

    우아한 스프링부트 세미나 project init

---
+++ SpringBoot/Uwahan-spring-boot/src/main/java/me/choi/UwahanSpringBootApplication.java
@@ -0,0 +1,13 @@
+package me.choi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UwahanSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UwahanSpringBootApplication.class, args);
+    }
+
+}
