2bed41d25e15e38b955fad74ee3a5b078f51f9bc
junwoo <33277588+mike6321@users.noreply.github.com>
2021-04-15 00:19:24 +0900

    (user-service) 다양한 방법의 기동

--- Spring/user-service/src/main/java/me/choi/userservice/UserServiceApplication.java
+++ Spring/user-service/src/main/java/me/choi/userservice/UserServiceApplication.java
@@ -13,3 +13,10 @@ public class UserServiceApplication {
     }
 
 }
+/**
+mvn spring-boot:run -Dspring-boot.run.jvmArguments='-Dserver.port=9003'
+
+mvn clean
+mvn compile package
+java -jar -Dserver.port=9004 ./target/user-service-0.0.1-SNAPSHOT.jar
+ * */
