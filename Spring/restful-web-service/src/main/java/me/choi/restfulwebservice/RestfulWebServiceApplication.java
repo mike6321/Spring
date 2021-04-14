6c8c0b28b6596f9e9dd7b4dca902ec1ac5b2c027
junwoo <33277588+mike6321@users.noreply.github.com>
2021-04-14 23:47:54 +0900

    (discoveryservice) init

---
+++ Spring/restful-web-service/src/main/java/me/choi/restfulwebservice/RestfulWebServiceApplication.java
@@ -0,0 +1,13 @@
+package me.choi.restfulwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulWebServiceApplication.class, args);
+    }
+
+}
