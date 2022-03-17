f1243557ec93e3eab93de5de7964c6fb84a80e79
mike6321 <mike6321@naver.com>
2022-03-17 15:14:54 +0900

    (advanced-spring) init project

---
+++ advanced-spring/src/main/java/me/choi/advancedspring/AdvancedSpringApplication.java
@@ -0,0 +1,13 @@
+package me.choi.advancedspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvancedSpringApplication.class, args);
+	}
+
+}
