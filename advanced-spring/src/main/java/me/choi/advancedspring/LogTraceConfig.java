c897a4d99c76ea498b5f1fe03c14275199fce984
mike6321 <mike6321@naver.com>
2022-03-20 16:48:00 +0900

    (advanced-spring) 쓰레드 로컬 동기화 - 적용

--- advanced-spring/src/main/java/me/choi/advancedspring/LogTraceConfig.java
+++ advanced-spring/src/main/java/me/choi/advancedspring/LogTraceConfig.java
@@ -1,7 +1,7 @@
 package me.choi.advancedspring;
 
-import me.choi.advancedspring.trace.logtrace.FieldLogTrace;
 import me.choi.advancedspring.trace.logtrace.LogTrace;
+import me.choi.advancedspring.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,7 +10,7 @@ public class LogTraceConfig {
 
     @Bean
     public LogTrace logTrace() {
-        return new FieldLogTrace();
+        return new ThreadLocalLogTrace();
     }
 
 }
