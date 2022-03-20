293259bbe352176f87ccb6a1230510bc2bd9f3fc
mike6321 <mike6321@naver.com>
2022-03-20 18:29:04 +0900

    (advanced-spring) 템플릿 메서드 패턴 - 예제2

--- advanced-spring/src/test/java/me/choi/advancedspring/trace/template/TemplateMethodTest.java
+++ advanced-spring/src/test/java/me/choi/advancedspring/trace/template/TemplateMethodTest.java
@@ -1,6 +1,9 @@
 package me.choi.advancedspring.trace.template;
 
 import lombok.extern.slf4j.Slf4j;
+import me.choi.advancedspring.trace.template.code.AbstractTemplate;
+import me.choi.advancedspring.trace.template.code.SubClassLogic1;
+import me.choi.advancedspring.trace.template.code.SubClassLogic2;
 import org.junit.jupiter.api.Test;
 
 @Slf4j
@@ -30,6 +33,18 @@ public class TemplateMethodTest {
         long endTime = System.currentTimeMillis();
         long resultTime = endTime - startTime;
         log.info("result Time = {}", resultTime);
+    }
+
+    /**
+     * 템플릿 메서드 패턴 적용
+     * */
+    @Test
+    void templateMethodV1() {
+        AbstractTemplate template1 = new SubClassLogic1();
+        template1.execute();
+
+        AbstractTemplate template2 = new SubClassLogic2();
+        template2.execute();
     }
 
 }
