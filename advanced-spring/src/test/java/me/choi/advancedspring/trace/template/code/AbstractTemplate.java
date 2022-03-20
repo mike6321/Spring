87f321243b2a11d576a3aaa424e71f7b5d6bf031
mike6321 <mike6321@naver.com>
2022-03-20 18:29:28 +0900

    (advanced-spring) 템플릿 메서드 패턴 - 예제2

---
+++ advanced-spring/src/test/java/me/choi/advancedspring/trace/template/code/AbstractTemplate.java
@@ -0,0 +1,20 @@
+package me.choi.advancedspring.trace.template.code;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public abstract class AbstractTemplate {
+
+    public void execute() {
+        long startTime = System.currentTimeMillis();
+        // 비즈니스 로직 실행
+        call();
+        // 비즈니스 로직 실행
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("result Time = {}", resultTime);
+    }
+
+    protected abstract void call();
+
+}
